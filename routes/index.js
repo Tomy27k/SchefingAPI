28dd5b8f4de69c4daf4aee9a4b2e2d79daf03e1d
Halil Toma <tomahalil1@gmail.com>
2022-10-31 00:38:24 +0300

    forum dit

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+let router1 = router.get('/', function(req, res, next) {
   res.render('index', { title: 'Schefing_API' });
   res.send('dsadas')
 });
+router.post('/', function(req, res, next) {
+  res.send('')
+})
 
 module.exports = router;
